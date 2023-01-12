4f4390bd2c844f5bfa9d49e0b9e424dfb518d0e8
Adzi <belkoadam@gmail.com>
2023-01-12 20:37:49 +1100

    linked list functions tested and working

--- script.js
+++ script.js
@@ -1,42 +1,111 @@
 // Factory fn that represents the full linked list
 const LinkedList = () => {
-    let headNode = null;
+    let head = null;
+    let nodeCount = 0;
 
     // Adds a new node to the end of the list
-    const append = (value) => {};
+    const append = (value) => {
+        const newNode = Node(value);
+        if (head === null) {
+            head = newNode;
+        } else {
+            let pointer = head;
+            while (pointer.nextNode) {
+                pointer = pointer.nextNode;
+            }
+            pointer.nextNode = newNode;
+        }
+        nodeCount++;
+    };
 
     // Adds a new node to the start of the list
-    const prepend = () => {};
+    const prepend = (value) => {
+        const newNode = Node(value, head);
+        head = newNode;
+        nodeCount++;
+    };
 
     // Returns the total number of nodes in the list
-    const size = () => {};
+    const size = () => nodeCount;
 
     // Returns the first node in the list
-    const head = () => {};
+    const headNode = () => head;
 
     // Returns the last node of the list
-    const tail = () => {};
+    const tailNode = () => {
+        let pointer = head;
+        while (pointer.nextNode) {
+            pointer = pointer.nextNode;
+        }
+        return pointer;
+    };
 
     // Returns boolean value based on passed value
-    const contains = (value) => {};
+    const contains = (value) => {
+        let pointer = head;
+        while (pointer) {
+            if (pointer.value === value) return true;
+            pointer = pointer.nextNode;
+        }
+        return false;
+    };
 
     // Returns the index of the node containing value, or null if not found
-    const find = () => {};
+    const findIndex = (value) => {
+        let index = 1;
+        let pointer = head;
+        while (pointer) {
+            if (pointer.value === value) {
+                return index;
+            } else {
+                pointer = pointer.nextNode;
+                index++;
+            }
+        }
+        return null;
+    };
 
     // Represent LinkedList objects as strings
-    const toString = () => {};
+    const toString = () => {
+        let pointer = head;
+        let string = "";
+        while (pointer) {
+            string += `( ${pointer.value} ) --> `;
+            pointer = pointer.nextNode;
+        }
+        return `${string}null`;
+    };
 
-    return { append, prepend, size, head, tail, contains, find, toString };
-};
-
-// Factory containing a value and a link to the next node
-const Node = (value, link) => {
     return {
-        value: value || null,
-        link: link || null,
+        append,
+        prepend,
+        size,
+        headNode,
+        tailNode,
+        contains,
+        findIndex,
+        toString,
     };
 };
 
-const nodeList = LinkedList();
-nodeList.append();
-nodeList.append(1);
+// Factory containing a value and a pointer to the next node
+const Node = (value, nextNode) => {
+    return {
+        value: value || null,
+        nextNode: nextNode || null,
+    };
+};
+
+const list = LinkedList();
+list.append(2);
+list.append(3);
+list.append(4);
+list.append(5);
+list.prepend(1);
+
+console.log(list.size());
+console.log(list.headNode());
+console.log(list.tailNode());
+console.log(list.contains(16));
+console.log(list.findIndex(2));
+console.log(list.toString());
